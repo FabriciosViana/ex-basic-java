fbb20e19f5dc5232fd6d4f9e28f1710efb6c5b7b
FabriciosViana <vianasfabricio@gmail.com>
2025-01-29 23:23:29 -0300

    resolving some questions

--- java-for-beginners-fkipper/Palindrome.java
+++ java-for-beginners-fkipper/Palindrome.java
@@ -4,34 +4,52 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the size of the list you want: ");
+//        String word = sc.nextLine();
+//        String reverseWord = sc.nextLine();
+//
+//        for (int i=(word.length()-1); i>=0; i--){
+//            reverseWord+=word.charAt(i);
+//        }
+//        if (reverseWord.equals(word)){
+//            System.out.println(word + " Is a palindrome");
+//        }else {
+//            System.out.println(word + "Is not a palndrome");
+//        }
+//
+//        System.out.println(word);
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the list you want: ");
         ArrayList<String> words = new ArrayList<String>();
-        int sizeArray = sc.nextInt();
+        System.out.println("How many words you would adding to the array? ");
+        int quantityWords = sc.nextInt();
+        sc.nextLine();
 
-        //Adding array elements
-        for (int i=0; i<sizeArray; i++){
-            System.out.println("Enter the string: ");
-            String addWords = sc.next();
-            words.add(addWords);
+        //Fill the arraylist
+        for (int i=0; i<quantityWords; i++){
+            System.out.println("Enter your text: ");
+            String inputWord = sc.nextLine();
+            words.add(inputWord);
+        }
+        for (String word: words){
+            String reversedWord = "";
+            for (int i=word.length()-1;i>=0;i--){
+                reversedWord+=word.charAt(i);
+            }
+
+            if (word.equals(reversedWord)){
+                System.out.println(word+" Is Palindrome");
+            }else{
+                System.out.println(word + " Is NOT a Palindrome");
+            }
         }
 
 
-        ArrayList<String> invertedWords = new ArrayList<String>();
-        for (int x=words.size()-1; x>=0 ;x--){
-            System.out.println(words.get(x));
-        }
 
 
 
 
-        System.out.println(words);
-
-
-
-
-
-
-        sc.close();
     }
 }
